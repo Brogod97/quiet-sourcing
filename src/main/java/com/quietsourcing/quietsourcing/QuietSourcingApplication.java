9b6f1282b8f3299f1f1954dc976a6b4155c9d770
Brogod97 <evergreen1286@gmail.com>
2024-03-04 22:33:17 +0900

    init

---
+++ src/main/java/com/quietsourcing/quietsourcing/QuietSourcingApplication.java
@@ -0,0 +1,13 @@
+package com.quietsourcing.quietsourcing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuietSourcingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuietSourcingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/quietsourcing/quietsourcing/QuietSourcingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quietsourcing.quietsourcing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuietSourcingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
