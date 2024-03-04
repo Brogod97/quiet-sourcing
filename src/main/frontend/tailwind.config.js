4793754e2d87288e6cec45b0f34a26e26b8ac47e
Brogod97 <evergreen1286@gmail.com>
2024-03-04 23:56:59 +0900

    chore: tailwind 추가 및 불필요 파일 삭제

---
+++ src/main/frontend/tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  mode: "jit",
+  purge: ["./src/**/*.{js,jsx}"],
+  content: ["./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
